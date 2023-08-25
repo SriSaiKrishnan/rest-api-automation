894f934692a215f32944d9edda1501a95fe0b5e5
SriSaiKrishnan <sai.krishnan@freshworks.com>
2023-08-25 21:25:13 +0530

    Jira Rest Api Practice

--- src/test/java/com/sai/jira/AddComment.java
+++ src/test/java/com/sai/jira/AddComment.java
@@ -3,21 +3,26 @@ package com.sai.jira;
 import com.sai.files.JiraPayload;
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 
 public class AddComment {
 
     @Test
-    public static void addComment(){
+    public static void addComment() {
 
         RestAssured.baseURI = "http://localhost:8080";
-
         SessionFilter session = new SessionFilter();
 
         //Create session
-     given()
-                .header("Content-Type" , "application/json")
+        given()
+                .relaxedHTTPSValidation()
+                .header("Content-Type", "application/json")
                 .body(JiraPayload.createSession())
                 .log()
                 .all()
@@ -31,20 +36,70 @@ public class AddComment {
                 .response()
                 .asString();
 
-//        given()
-//                .queryParam("key","10001")
-//                .header("Content-Type" , "application/json")
-//                .body(JiraPayload.addComment())
-//                .log()
-//                .all()
-//                .filter(session)
-//                .when()
-//                .post("/api/2/issue/{key}/comment")
-//                .then()
-//                .log()
-//                .all();
+        //Add comment
+        String expectedMessage = "Hi How are you?";
+        String addCommentResponse = given().
+                pathParam("key", "10101")
+                .log()
+                .all()
+                .header("Content-Type", "application/json")
+                .body(JiraPayload.addComment())
+                .filter(session)
+                .when()
+                .post("/rest/api/2/issue/{key}/comment")
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(201)
+                .extract()
+                .response()
+                .asString();
 
+        JsonPath js = new JsonPath(addCommentResponse);
+        String commentId = js.getString("id");
 
+        //Add Attachment
+        given()
+                .header("X-Atlassian-Token", "no-check")
+                .filter(session)
+                .pathParam("key", "10101")
+                .header("Content-Type", "multipart/form-data")
+                .multiPart("file", new File("jira.txt"))
+                .when()
+                .post("rest/api/2/issue/{key}/attachments")
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(200);
+
+        //Get Issue
+        String issueDetails = given()
+                .filter(session)
+                .pathParam("key", "10101")
+                .queryParam("fields", "comment")
+                .log()
+                .all()
+                .when()
+                .get("/rest/api/2/issue/{key}")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .response()
+                .asString();
+        System.out.println(issueDetails);
+        JsonPath js1 = new JsonPath(issueDetails);
+        int commentsCount = js1.getInt("fields.comment.comments.size()");
+
+        for (int i = 0; i < commentsCount; i++) {
+            String commentIdIssue = js1.get("fields.comment.comments[" + i + "].id").toString();
+            if (commentIdIssue.equalsIgnoreCase(commentId)) {
+                String message = js1.get("fields.comment.comments[" + i + "].body").toString();
+                System.out.println(message);
+                Assert.assertEquals(message, expectedMessage);
+            }
+        }
     }
-
 }
